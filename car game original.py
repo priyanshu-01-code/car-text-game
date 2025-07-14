9be45dcf06ea13c8ac2742ca86565f4fad8b1118
Priyanshu Pandit <pandit.priyanshu0978@gmail.com>
2025-07-14 21:22:58 +0530

    Add files via upload

---
+++ car game original.py
@@ -0,0 +1,27 @@
+#What I have to do is to make a program which should do whar? For every command given it should have a permanent symbol like an arrow sign. It is tough.
+
+instructions_to_start_the_car = """type 'START' to start the car
+type 'STOP' to stop the car
+type 'QUIT' to end the game
+(you can actually type the same words in any case. It is just representational.)"""
+
+print(instructions_to_start_the_car)
+
+A = 1
+while A <= 1:
+    y = input('>').upper()
+
+    if y == 'START':
+        print("The car has started. VROOOOOOOM!!!")
+    elif y == 'STOP':
+        print('The car has stopped.')
+    elif y == 'QUIT':
+        print('Hope you had a nice session.')
+        A += 2
+        
+    else:
+        print("Are you blind? Can't you read the instructions?")
+
+
+ 
+        
